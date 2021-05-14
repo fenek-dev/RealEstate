be5b1d5869ff0b772fbd8f64e9e81b6c0bdf7fa7
MaksFenek <makstreonin@gmail.com>
2021-05-14 15:56:54 +0300

    Added auth folder

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,12 +3,14 @@ import { ProductModule } from './product/product.module'
 import { MongooseModule } from '@nestjs/mongoose'
 import { ConfigModule } from '@nestjs/config'
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGO_URI),
     ProductModule,
     UserModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
