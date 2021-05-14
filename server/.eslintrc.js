8f8d4bc08d3f4723240a6073b4f2de82b5786020
MaksFenek <makstreonin@gmail.com>
2021-05-14 13:29:28 +0300

    Initial initialization

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
@@ -21,4 +22,4 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
   },
-};
+}
