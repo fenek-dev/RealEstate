41db308a6a9e46ea396d40776534787b560f97eb
MaksFenek <makstreonin@gmail.com>
2021-05-21 14:40:27 +0300

    Added search component

--- src/client/pages/_app.tsx
+++ src/client/pages/_app.tsx
@@ -1,5 +1,5 @@
 import {wrapper} from '../redux/store'
-import '../styles/globals.css'
+import '../styles/globals.scss'
 
 function MyApp({Component, pageProps}) {
   return <Component {...pageProps} />
--- src/client/pages/index.tsx
+++ src/client/pages/index.tsx
@@ -2,7 +2,11 @@ import {END} from 'redux-saga'
 import {wrapper} from '../redux/store'
 import Head from 'next/head'
 import MainLayout from '../layouts/Main'
-
+import {Typography} from 'antd'
+import Image from 'next/image'
+import styles from '../styles/index.module.scss'
+import Search from '../components/Search'
+const {Title, Text} = Typography
 export default function Home() {
   return (
     <MainLayout>
@@ -14,6 +18,27 @@ export default function Home() {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <section className={styles.welcome}>
+        <div className={styles.welcomeTitle}>
+          <Text>Digital Estate is</Text>
+          <Title level={1}>The real estate marketplace</Title>
+          <Title level={4}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit
+          </Title>
+        </div>
+        <div style={{width: '100%'}}>
+          <Image
+            loading="lazy"
+            src="/welcome.jpg"
+            width="100%"
+            height="50%"
+            layout="responsive"
+            alt="Digital Estate"
+          />
+        </div>
+
+        <Search className={styles.search} />
+      </section>
     </MainLayout>
   )
 }
